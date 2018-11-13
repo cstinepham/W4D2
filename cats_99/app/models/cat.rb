5fded5a6e66bab4b5e98b1ee562882f3ea354e37
cstinepham <cstinepham@gmail.com>
2018-11-13 12:20:15 -0800

    Create CatsController with #index and #show

--- cats_99/app/models/cat.rb
+++ cats_99/app/models/cat.rb
@@ -1,2 +1,9 @@
 class Cat < ApplicationRecord
+  validates :birth_date, presence: true
+  validates :color, presence: true
+  validates :name, presence: true
+  validates :sex, presence: true
+  validates :description, presence: true
+
+  
 end
--- cats_99/db/seeds.rb
+++ cats_99/db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cat.create!(birth_date: '2018/11/12', color: 'yellow', name: 'Garfield', sex: 'M', description: 'fat and lazy')
+Cat.create!(birth_date: '2013/11/12', color: 'black', name: 'Luna', sex: 'F', description: 'sleepy all the time')
+Cat.create!(birth_date: '2011/1/12', color: 'White', name: 'Snowball', sex: 'F', description: 'Really nice and fuzzy')
+Cat.create!(birth_date: '2018/11/12', color: 'Tabby', name: 'Princess', sex: 'M', description: 'acts like a princess')
+Cat.create!(birth_date: '2018/11/12', color: 'Spotted', name: 'Ryan', sex: 'M', description: 'Likes to climb')
