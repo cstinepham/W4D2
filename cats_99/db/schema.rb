70f7ad08e2902e8c20f26f2b6f05ebdca4482525
cstinepham <cstinepham@gmail.com>
2018-11-13 18:02:24 -0800

    Reseed database and remove uniqueness from cat_id index

--- cats_99/db/schema.rb
+++ cats_99/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_14_010457) do
+ActiveRecord::Schema.define(version: 2018_11_14_013627) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2018_11_14_010457) do
     t.string "status", default: "PENDING", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["cat_id"], name: "index_cat_rental_requests_on_cat_id", unique: true
+    t.index ["cat_id"], name: "index_cat_rental_requests_on_cat_id"
   end
 
   create_table "cats", force: :cascade do |t|
--- cats_99/db/seeds.rb
+++ cats_99/db/seeds.rb
@@ -6,8 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Cat.create!(birth_date: '2018/11/12', color: 'yellow', name: 'Garfield', sex: 'M', description: 'fat and lazy')
-Cat.create!(birth_date: '2013/11/12', color: 'black', name: 'Luna', sex: 'F', description: 'sleepy all the time')
+Cat.create!(birth_date: '2018/11/12', color: 'Yellow', name: 'Garfield', sex: 'M', description: 'fat and lazy')
+Cat.create!(birth_date: '2013/11/12', color: 'Black', name: 'Luna', sex: 'F', description: 'sleepy all the time')
 Cat.create!(birth_date: '2011/1/12', color: 'White', name: 'Snowball', sex: 'F', description: 'Really nice and fuzzy')
 Cat.create!(birth_date: '2018/11/12', color: 'Tabby', name: 'Princess', sex: 'M', description: 'acts like a princess')
 Cat.create!(birth_date: '2018/11/12', color: 'Spotted', name: 'Ryan', sex: 'M', description: 'Likes to climb')
+Cat.create!(birth_date: '1995/02/08', color: 'Rainbow', name: 'Chase', sex: 'M', description: 'Refuses to switch after 15 minutes are up')
+Cat.create!(birth_date: '1993/08/06', color: 'Spotted', name: 'Christine', sex: 'F', description: 'Likes sunbathing and eating ice cream while watching netflix')
+
+
+
+CatRentalRequest.create!(cat_id: 1, start_date: '2018/10/31', end_date: '2018/11/1', status: 'APPROVED')
+CatRentalRequest.create!(cat_id: 6, start_date: '2010/01/20', end_date: '2015/12/24', status: 'APPROVED')
+# CatRentalRequest.create!(cat_id: 6, start_date: '2012/01/05', end_date: '2013/06/15', status: 'DENIED')
